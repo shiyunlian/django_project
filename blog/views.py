96d8f2ec07e847ee377559cbf145734879dbcf9e
shiyunlian <shiyunlian00@gmail.com>
2023-01-02 18:01:28 -0800

    add templates

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+
+# create a hard coded data, a list of dictionary
+posts = [
+    {
+        'author': "Seven",
+        'title': 'Blog 1',
+        'content': 'First Blog',
+        'date_posted': 'August 27, 2022'
+    },
+    {
+        'author': "Elsa",
+        'title': 'Blog 2',
+        'content': 'First Blog too',
+        'date_posted': 'August 28, 2022'
+    }
+]
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    # context is a dictionary, the key of the context is posts, which is accessible from the templates, pass posts data to posts key
+    context = {'posts': posts}
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
